233522ae109179e544f212925d938b36ea9e3239
Vicente Luchi <vicente.luchi@gmail.com>
2015-09-26 21:29:51 -0300

    Parse the content of the chat file

--- chatterbot-lfa/chatterbot.py
+++ chatterbot-lfa/chatterbot.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def main(chat_script_path):
     try:
-        chat_script_file = open(chat_script_path)
-        read_content = chat_script_file.read()
-        chat_script_file.close()
+        read_content = read_file(chat_script_path)
     except IOError:
-        print("File not found.")
+        print("File not found. End of the program.")
+    else:
+        parsed_content = parse_chat_content(read_content)
+        print(parsed_content)
+
+def read_file(chat_script_path):
+    """Opens the file"""
+    chat_script_file = open(chat_script_path)
+    read_content = chat_script_file.read()
+    chat_script_file.close()
+    return read_content
+
+
+def parse_chat_content(read_content):
+    """Parses the read content, removing the format characters"""
+
+    # Removes the encoding markup
+    read_content = read_content.lstrip('\ufeff')
+
+    # Removes whitespace and tabs from the strings
+    # adding them to the result variable
+    parsed_content = []
+    for line in read_content.splitlines():
+        line = line.strip(' \t')
+        parsed_content.append(line)
+
+    return parsed_content
